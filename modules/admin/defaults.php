59994a6f4b3ed9df88bc6ef596249da9b60323c3
Joonas Meriläinen <merilainenjoonas@gmail.com>
2018-11-27 12:17:24 +0200

    Change admin defaults to theme defaults

--- modules/admin/defaults.php
+++ modules/admin/defaults.php
@@ -1,13 +1,3 @@
 <?php
 
 namespace Frc\WP\Base\Admin\Defaults;
-
-/**
- * Remove links from images by default
- *
- * @link http://andrewnorcross.com/tutorials/stop-hyperlinking-images/
- */
-function remove_image_links() {
-    update_option('image_default_link_type', 'none');
-}
-add_action('after_switch_theme', __NAMESPACE__ . '\\remove_image_links');
--- modules/theme/defaults.php
+++ modules/theme/defaults.php
@@ -1,3 +1,13 @@
 <?php
 
 namespace Frc\WP\Base\Theme\Defaults;
+
+/**
+ * Remove links from images by default
+ *
+ * @link http://andrewnorcross.com/tutorials/stop-hyperlinking-images/
+ */
+function remove_image_links() {
+    update_option('image_default_link_type', 'none');
+}
+add_action('after_switch_theme', __NAMESPACE__ . '\\remove_image_links');
