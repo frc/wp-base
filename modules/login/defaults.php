aff8dbd55276b514bd79a50a66b3bca943cae400
Joonas Meriläinen <merilainenjoonas@gmail.com>
2019-02-20 11:13:20 +0200

    Add support for customising login logos & styles + tweak default styles

--- modules/login/defaults.php
+++ modules/login/defaults.php
@@ -17,10 +17,28 @@ if ( defined( 'WP_ENV' ) && WP_ENV !== 'production' ) {
  * @link http://codex.wordpress.org/Plugin_API/Filter_Reference/login_message
  */
 function add_development_login_message($message) {
-    return $message . '<p class="message" style="border-left: 4px solid #FFBA00;">' . wp_kses(__('<strong>Note:</strong> You are about to login to a ' . WP_ENV . ' environment', 'frc-wp-base'), 'strong') . '</p>';
+    return '<p class="message" style="border-left: 4px solid #FFBA00;">' . wp_kses(__('<strong>Note:</strong> You are about to login to a <strong>' . WP_ENV . '</strong> environment', 'frc-wp-base'), 'strong') . '</p>' . $message;
 }
 
 function login_styles() {
     wp_enqueue_style('base/login', helpers\asset_path('styles/login.css'), false, null);
+
+    $options = helpers\get_options(__FILE__);
+
+    if ( isset($options['logo']) && $options['logo'] === false ) {
+        wp_add_inline_style('base/login', "
+.login h1 a {
+    display: none;
+}
+        ");
+    }
+
+    if ( !empty($options['logo'])  ) {
+        wp_add_inline_style('base/login', "
+.login h1 a {
+    background-image: url('{$options['logo']}');
+}
+        ");
+    }
 }
 add_action( 'login_enqueue_scripts', __NAMESPACE__ . '\\login_styles' );
--- modules/plugin/auth0.php
+++ modules/plugin/auth0.php
@@ -6,5 +6,60 @@ use Frc\WP\Base as helpers;
 
 function enqueue_styles() {
     wp_enqueue_style('base/login-auth0', helpers\asset_path('styles/login-auth0.css'), false, null);
+
+    $options = helpers\get_options(__FILE__);
+
+    if ( !empty($options['primaryColor']) ) {
+        wp_add_inline_style('base/login-auth0', "
+.auth0-lock.auth0-lock .auth0-lock-header-bg.auth0-lock-header-bg {
+    background: {$options['primaryColor']};
 }
-add_action( 'login_enqueue_scripts', __NAMESPACE__ . '\\enqueue_styles' );
+        ");
+    }
+
+    if ( isset($options['header']) && $options['header'] === false ) {
+        wp_add_inline_style('base/login-auth0', "
+#auth0-login-form {
+    min-height: 113px;
+}
+.auth0-lock.auth0-lock .auth0-lock-header {
+    display: none;
+}
+        ");
+    }
+}
+add_action( 'login_enqueue_scripts', __NAMESPACE__ . '\\enqueue_styles', );
+
+function login_message($html) {
+
+    $options = helpers\get_options(__FILE__);
+
+    wp_register_script( 'dummy-handle-footer', '', [], '', true );
+    wp_enqueue_script( 'dummy-handle-footer'  );
+
+    if ( !empty($options['language']) ) {
+        wp_add_inline_script( 'dummy-handle-footer', "wpAuth0LockGlobal.settings.language = '{$options['language']}';");
+    }
+
+    wp_add_inline_script( 'dummy-handle-footer', "wpAuth0LockGlobal.settings.theme = {};");
+
+    if ( !empty($options['logo']) ) {
+        wp_add_inline_script( 'dummy-handle-footer', "wpAuth0LockGlobal.settings.theme.logo = '{$options['logo']}';");
+    }
+
+    if ( isset($options['logo']) && $options['logo'] === false ) {
+        wp_add_inline_script( 'dummy-handle-footer', "wpAuth0LockGlobal.settings.theme.logo = '';");
+    }
+
+    if ( !empty($options['primaryColor']) ) {
+        wp_add_inline_script( 'dummy-handle-footer', "wpAuth0LockGlobal.settings.theme.primaryColor = '{$options['primaryColor']}';");
+    }
+
+    if ( isset($options['title']) ) {
+        wp_add_inline_script( 'dummy-handle-footer', "wpAuth0LockGlobal.settings.languageDictionary = { title: '{$options['title']}' };");
+    }
+
+    return $html;
+}
+
+add_filter( 'login_message', __NAMESPACE__ . '\\login_message');
